e0661fff7a626c0d2114103884aba4c0f009829c
carlitos_madrigal <madrigalgallego.jc@gmail.com>
2022-03-17 13:30:01 +0100

    Sin terminar

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // 1. Initialize leaflet and center the city of Los Angeles
-var map = L.map("mapid").setView(/* Coordinates goes here */, 10);
+const lat = 34.052235;
+const long = -118.243683;
+var map = L.map("mapid").setView([lat, long], 13);
 
 // 2. Add Layer from mapbox
-const MAPBOX_TOKEN = '';
+const MAPBOX_TOKEN = 'pk.eyJ1IjoiY2FybG9zbWFkcmlnYWw4MyIsImEiOiJjbDB0emF0M2IwYjg4M2NrYmVrY3hsemxyIn0.jIWkI8LElkIqA8txJPwplg';
 
 L.tileLayer(
   `https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}`,
@@ -17,9 +19,22 @@ L.tileLayer(
   }
 ).addTo(map);
 
+fetch('https://api.metro.net/vehicle_positions/bus?output_format=json')
+  .then(response => response.json())
+  .then(data => data.entity.map(element => {
+    const lat = element.vehicle.position.latitude;
+    const long = element.vehicle.position.longitude;
+    const resultado = [lat, long];
+    console.log(resultado)
+  }));
+  
+
 // 3. By using the metro API make a fetch to retrieve the data
 
 // 4. Display in your map the public transports
-L.marker(/* Marker's coordinates */).bindPopup(/* Marker Text*/).addTo(map);
+L.marker(resultado.map(element) => {
+
+}).bindPopup(/* Marker Text*/).addTo(map);
 
 // 5. Create a code to refresh each 5 seconds and retrieve the new positions of the public transports
+
